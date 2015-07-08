dfd3d10e658e40f9b423a2ae82938fac306e843f
Xavier Banks <xavier95@knights.ucf.edu>
2015-07-08 17:35:20 -0400

    MongoDB

--- Web/WebAPI/app.js
+++ Web/WebAPI/app.js
@@ -1,11 +1,14 @@
-
+var PORT = 8081;
 // When requiring the routes folder, it automatically gets the index.js file
 var routes = require('./routes');
 var site = require('./routes/site');
-var leaderboard = require('./routes/leaderboard');
+var api = require('./routes/api');
+// Neccessary libs
 var express = require('express');
 var logger = require('morgan');
 var http = require('http');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 
 // Creates the express app that we use for most of our routing operations
 var app = express();
@@ -15,18 +18,21 @@ var path = require('path');
 // Some middleware
 app.set('view engine', 'jade');
 app.use(logger('dev'));
+app.use(bodyParser.json());
+// Load files from the Schema 
+
 
 // app.use(express.static(__dirname + '/public'));
-
-// Rounting for the different pages
+// Routing for the different pages
 app.get('/', site.index);
-app.get('/api/', leaderboard.leaderboard);
+app.get('/api/', api.level);
+app.post('/api/', api.addEntry);
+
 
 // This will be used once the DB is setup and routing is configured
 // app.get('/api/:timeframe/:type', leaderboard.api);
-
 app.get('/:page', site.page);
 
-http.createServer(app).listen(8080, function () {
-    console.log("Listening on port: 8080");
+http.createServer(app).listen(PORT, function () {
+    console.log("Listening on port: " + PORT);
 });
