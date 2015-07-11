54f56a14ccb3da491c9662d8aca5a3aa7e8596b8
Xavier Banks <xavier.xb95@gmail.com>
2015-07-10 23:07:43 -0400

    Changes to api

--- Web/WebAPI/routes/api.js
+++ Web/WebAPI/routes/api.js
@@ -26,7 +26,13 @@ exports.allLevels = function(req, res){
 exports.getLevel = function(req, res){
     var level = req.level;
     
-    res.send('All leaderboards from Level: ' + level);
+    // res.send('All leaderboards from Level: ' + level);
+    
+    mongoose.model('Level').find(function(err, lvl){
+        mongoose.model('Level').populate(lvl, {path: 'Leaderboards'}, function(err, lvl){
+            res.send(lvl);
+        });
+    });
 };
 
 exports.getBoard = function(req, res){
@@ -40,8 +46,33 @@ exports.getBoard = function(req, res){
 exports.addEntry = function(req, res){
 
     var json = req.body;
+    var names = ['xavier', 'guf', 'marcus', 'bobby'];
     
-    var entry = new EntryModel({
-        Initials : "abc"
+    var board = new Leaderboard({
+        Name : "Leaderboard 1",
+        Description : "This is a test leaderboard",
+        Entries : []
     });
+    
+    for(var i = 0; i < 10; i++){
+        var entry = new EntryModel({
+            Initials : names[Math.floor(Math.random() * 4)],
+            Score : Math.floor((Math.random() * 1000) + 1),
+            LevelTime : Math.floor(Math.random() * 360),
+            Date : Date.now()
+        });
+        entry.save();
+        board.entries.push(entry);
+    }
+    
+    board.save();
+    
+    var level = new Level({
+        Name : "Level 1",
+        Description : "This is Level one of our project",
+        Leaderboards : []
+    });
+    
+    level.leaderboards.push(board);
+    level.save();
 };
--- Web/WebAPI/routes/schemas/level.js
+++ Web/WebAPI/routes/schemas/level.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var ObjectId = Schema.ObjectId;
 var Leaderboard = mongoose.model("Leaderboard");
 
 var Level = new Schema({
     Name    :   String,
     Description :   String,
-    Leaderboards    :   [Leaderboard]
+    Leaderboards    :   [{
+        type : ObjectId,
+        ref : 'Leaderboard'
+    }]
 });
 
 mongoose.model('Level', Level);
-exports.Model = Level;
